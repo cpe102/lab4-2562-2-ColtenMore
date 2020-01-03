8a7d6f1f8c24364460c9c16393d4a80d6938a457
Aungpao <aungpao321@gmail.com>
2020-01-03 16:41:09 +0700

    v1

--- lab4_2.cpp
+++ lab4_2.cpp
@@ -1,12 +1,28 @@
 #include<iostream>
-
+#include<cmath>
 using namespace std;
 
 //Write function findDistance() here
+double findDistance(double u,double a,double t){
+	double s = u*t+0.5*a*t*t;
+	return s;
+}
 
 int main(){
 
   //Calling findDistance() using test cases
-
+  cout<<"u = ";
+	double u;
+	cin>>u;
+	
+	cout<<"a = ";
+	double a;
+	cin>>a;
+	
+	cout<<"t = ";
+	double t;
+	cin>>t;
+	
+	cout<<"s= "<< findDistance(u,a,t);
   return 0;
 }
--- lab4_3.cpp
+++ lab4_3.cpp
@@ -1,1 +1,23 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int findDivisor(int x){
+	int y=2;
+	while(y>1){
+		if(x%y==0){
+			return y;
+		}
+		else{
+			y++;
+		}
+	}
+}
+int main(){
+	cout<<"Input = ";
+	int x;
+	cin>>x;
+	cout<<"Output = "<<findDivisor(x);
+	return 0;
+}
 
